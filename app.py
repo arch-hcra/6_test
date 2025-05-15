7e3f660eea60fa6c234f7fa640bb6e6e1459815e
archhcra <bomcom@bk.ru>
2025-05-15 16:07:18 +0300

    FRST_3

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify(message="Hello, World!")
+    return "Hello, World!"
+
+@app.errorhandler(400)
+def bad_request(error):
+    return "Bad request", 400
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
